c642ba2486c5497a0d52bbbc5134b9dde57b9fc5
the-swerve <jayrbolton@gmail.com>
2013-05-16 17:09:37 -0700

    Added the functionality of responding to the specific channel or query-er. The bot also now correctly passes 'messenger' to its callbacks.

--- botinator.py
+++ botinator.py
@@ -1,67 +1,103 @@
 import socket, threading, datetime, re, sys
 
 class Bot:
-	def __init__(self, serv, port, nick, username, ircname):
+	def __init__(self, server_name, port=6667):
+		"""
+		Pass the server_name (eg. irc.freenode.net), and the port (optional;
+		defaults to 6667)
+		"""
 		self.irc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-		self.irc.connect((serv,port))
-		self.irc.send('USER ' + ((username + ' ')*3) + ':' + ircname + '\r\n') 
-		self.irc.send('NICK ' + nick + '\r\n')
+		self.server_name = server_name
+		self.port = port
+		self.irc.connect((server_name, int(port)))
 		self.bindings = {}
 		self.conditions = {}
 		self.state = {}
 
 	def join(self,chan):
-		"Join a channel"
 		self.chan = chan
 		self.irc.send ('JOIN ' + chan + '\r\n')
 		return self
 
+	def nick(self,n):
+		self.nick = n
+		self.irc.send('NICK ' + n + '\r\n')
+	
+	def user_and_ircname(self,u='',n=''):
+		self.user = u
+		self.ircname = n
+		self.irc.send('USER ' + ((u + ' ')*3) + ':' + n + '\r\n')
+	
 	def bind(self,pattern,response):
 		"Bind a matched regex to a response"
 		self.bindings[pattern] = response
 		return self
 
 	def when(self,condition,response):
-		"Bind a predicate function to a response"
+		"""
+		Bind a predicate function to a response.
+
+		The predicate automatically toggles a switch in the bot: by default, the
+		toggle is off. When the predicate is true and the toggle is off, the toggle
+		goes on and the response is fired. When the predicate is true and the
+		toggle is on, then the response is not fired. When the predicate is false
+		and the toggle is true, then the toggle goes false. When the predicate is
+		false and the toggle, is off, nothing happens.
+
+		This allows the bot to speak up for an event only once. For example, if the
+		bot were to say 'Time to go!' when it's 5pm every day, we'd want that to
+		fire only once, and not a million times during the time between 5:00:00 and
+		5:00:59.
+		"""
 		self.conditions[condition] = response
 		return self
 
-	def __repr__(self): return "botbotbotbotbot"
+	def __repr__(self):
+		return str(self.state)
 
-	def pong(self): self.irc.send("PONG :pings\n")
+	def pong(self, data):
+		pong = "PONG " + data.split()[1] + "\r\n"
+		print('Ponging with: ' + pong)
+		self.irc.send(pong)
 
-	def respond(self,response,*args):
+	def respond(self,channel,response,*args):
 		"""
 		Send a response, which may be a function returning a string or just a string
 		Response function must have two parameters: list of regex matches and the
 		messenger nick.
 		"""
-		if hasattr(response, '__call__'): # is function object?
+		if hasattr(response, '__call__'): # response is a callback function
 			try:
-				print('Responding: ' + response(*args))
-				self.irc.send('PRIVMSG '+self.chan+' :'+response(*args)+'\n') # apply function to regex matches
+				res = response(*args)
 			except:
-				print('Reponse threw exception: ', sys.exc_info()[0], 'line' + str(sys.exc_info()[2].tb_lineno))
+				print('Reponse callback threw exception: ', sys.exc_info()[0], 'line' + str(sys.exc_info()[2].tb_lineno))
 				return
 		else:
-			print('Responding: ' + response)
-			self.irc.send('PRIVMSG '+self.chan+' :'+response+'\n') # just send string
+			res = response
+
+		print('Responding: ' + res)
+		self.irc.send('PRIVMSG '+ channel + ' :' + res + '\r\n')
 
 	def live(self):
-		"Main event loop. Run after everything else is configured. Ctrl-C to end."
-		# How could someone modify the bot while it is looping this method to
-		# dynamically play with its behavior?
+		"Initialize the event loop thread."
 		while True:
 			data = self.irc.recv(4096)
 			print(data)
-			if data.find("PING :") != -1: self.irc.send("PONG :pings\n")
+			if data.find("PING") != -1:
+				self.pong(data)
 			# Respond to messages.
-			elif data.find(' PRIVMSG ' + self.chan) != -1:
+			elif data.find('PRIVMSG') != -1:
+				pieces = data.split()
+				channel = pieces[2]
+				messenger = re.search(':(.*)!', pieces[0]).group(1)
+				if channel == self.nick: # querying
+					channel = messenger
 				for pattern, response in self.bindings.iteritems():
 					matches = re.findall(pattern, data)
 					if matches:
 						print("Found match: " + str(pattern))
-						self.respond(response,matches,'john')
+						self.respond(channel,response,matches,messenger)
 			# Check conditions.
 			for condition, response in self.conditions.iteritems():
-				if condition(): self.respond(response)
+				if condition(): 
+					self.respond(channel,response,messenger)
--- readme.md
+++ readme.md
@@ -1,14 +1,17 @@
 
-# ~~~~BOTINATOR~~~~
+# Botinator
 
-## Super easy irc bot api
+## Super declarative IRC bot API
 
 ## Create your bot:
 
-Pass server, port, nick, username, ircname, and channel.
+Set up server, port, nick, username, ircname, and channel.
 
-	from botinator import *
-	bot = Bot('irc.freenode.net','6667', 'imabot', 'bot', 'Winston Churchill', '#your_awesome_channel')
+	import botinator
+	bot = botinator.Bot('irc.freenode.net', 6669)
+	bot.user_and_ircname('bot', 'Winston Churchill')
+	bot.nick('bottymcbotterson')
+	bot.join('#your_channel')
 
 ## Basic behavior:
 Bind a pattern to a response:
@@ -18,22 +21,28 @@ Bind a pattern to a response:
 
 The pattern can be a regex:
 
-	bot.bind(r.'[(hello)|(hi)|(hey)]', 'well hi there')
+	bot.bind(r'[(hello)|(hi)|(hey)]', 'well hi there')
 
 Regex syntax: http://docs.python.org/2/library/re.html#regular-expression-syntax
 
 ## More complex behavior:
-Map your regexes to callback functions that take the matched strings
-from your regex and return an irc command.
+Instead of responding with a string, botinator can respond with a callback
+function that allows more complex behavior.
+
+This callback function takes the regular expression matches and must return a
+string as its response.
+
+The callback function also has access to the nickname of the messenger who
+initiated the callback.
 
 	def get_time(matches, messenger):
 		return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
 
-	bot.bind('wat time', get_time)
+	bot.bind('hey bot!: wat time', get_time)
 
 The 'matches' parameter passed into your callback functions will be a list of successfuly matched strings from your regex.
 
-	bot.bind(r'yo bot: \d+ \+ \d+' : add_yr_numbers)
+	bot.bind(r'yo bot: \d+ \+ \d+', add_yr_numbers)
 
 	def add_yr_numbers(matches, messenger):
 		try:
@@ -41,8 +50,6 @@ The 'matches' parameter passed into your callback functions will be a list of su
 			right = int(matches[1])
 			return str(left + right)
 		except: return 'wat'
-
-You must also declare the 'messenger' for your callback functions, which is just the message author's name.
 
 Exceptions in your callback functions won't kill your bot if you don't catch em, but will just be printed. He's a toughy.
 
@@ -64,6 +71,9 @@ predicate function to a specific response:
 
 ### todo
 
+* Ordering of pattern matching 
 * Extract channel and then respond to that specific channel
 * Extract messenger name and pass to response function
-* Dynamically update bot while running event loop?
+* Get 'when()' method working with toggling.
+* Make the bot's state modifiable.
+* Dynamically update bot while running event loop.
